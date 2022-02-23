19c64f55facb7faede5334621b18251f37abdc35
Jahid <jahidhasan.developer@gmail.com>
2022-02-23 06:11:10 +0600

    change api

--- src/helpers/api_helper.js
+++ src/helpers/api_helper.js
@@ -6,9 +6,11 @@ const token = accessToken
 
 //apply base url for axios
 // 
-const API_URL = "/api";
+// const API_URL = "/api";
 
-// const API_URL = "http://localhost:4100"
+const API_URL = "https://tranding-project.herokuapp.com/";
+
+// const API_URL = "http://localhost:4500"
 
 const axiosApi = axios.create({
   baseURL: API_URL,
